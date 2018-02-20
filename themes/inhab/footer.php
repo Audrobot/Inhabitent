2e7ca4435e34e7ffbe34ba653e7f3c02688ef30b
Audrobot <kornickiaa@gmail.com>
2018-02-20 13:34:32 -0800

    styling changes, plug-ins

--- themes/inhab/footer.php
+++ themes/inhab/footer.php
@@ -12,11 +12,33 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+					</div>
+				
+					  <div class="footer">
+                <div class="contact-info">
+                    <h3>CONTACT INFO</h3>
+                    <p><span class="fa fa-envelope" aria-hidden="true"></span>info@inhabitent.com</p>
+                    <p><span class="fa fa-phone" aria-hidden="true"></span>778-456-7891</p>
+                    <p><span class="fab fa-facebook-square" aria-hidden="true"></span>
+										<span class="fab fa-twitter-square" aria-hidden="true"></span>
+                    <span class="fab fa-google-plus-square" aria-hidden="true"></span></p>
+                </div>
+                <div class="business-hours">
+                    <h3>BUSINESS HOURS</h3>
+                    <p><span class="day">Monday-Friday:</span> 9am to 5pm</p>
+                    <p><span class="day">Saturday:</span> 10am to 2pm</p>
+                    <p><span class="day">Sunday:</span> Closed</p>
+                </div>  
+                <div class = "footer-image">
+                    <a href= "/"> 
+                        <img src=<?php echo get_template_directory_uri() . "/images/logos/inhabitent-logo-text.svg" ?> alt="">
+                    </a>
+                </div>
+                <div class="copyright-container">
+					<div class="copyright">Copyright © 2017 inhabitent</div>
 
 		<?php wp_footer(); ?>
 
 	</body>
 </html>
+	
