b9a4059cd9642237f8494d04e284cf97d0ae46b0
Audrobot <kornickiaa@gmail.com>
2018-02-23 15:43:16 -0800

    styling changes

--- themes/inhab/front-page.php
+++ themes/inhab/front-page.php
@@ -87,12 +87,12 @@ get_header(); ?>
           <?php the_post_thumbnail( 'large' ); ?>
         <?php endif; ?>
 
-        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-        
         <div class="entry-meta">
           <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
         </div><!-- .entry-meta -->
+
+         <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
         </article>
       <?php endforeach; wp_reset_postdata(); ?>
 
