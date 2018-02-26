d9e57b7ac3ab524da0c36bd1c225d9ff7a174dbe
Audrobot <kornickiaa@gmail.com>
2018-02-25 23:17:45 -0800

    modify adventure box, shop item text

--- themes/inhab/template-parts/products-archives.php
+++ themes/inhab/template-parts/products-archives.php
@@ -1,11 +1,13 @@
 
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('shop-item'); ?>>
+  
   <?php if ( has_post_thumbnail() ) : ?>
     <?php the_post_thumbnail( 'large' ); ?>
   <?php endif; ?>
+  
   <div class="shop-item-text">
-    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', 
+    <?php the_title( sprintf( '<h2 class="shop-item-title"><a href="%s" rel="bookmark">', 
     esc_url( get_permalink() ) ), '</a></h2>' ); ?>
     <span class="shop-item-ellipses"></span>
     <span><?php echo CFS()->get( 'price' ); ?></span>
